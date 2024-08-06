c79974ebe1523cf5c7006bfd56a7197b6ad1bc91
Julius Stein <julius.stein@datarobot.com>
2024-08-06 12:41:54 -0400

    publishing to add env vars in platform

--- streamlit_launcher.py
+++ streamlit_launcher.py
@@ -1,42 +1,59 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from generative_wrangling import summarize_text, rephrase_text, categorize_text, encode_variables, translate_text, analyze_sentiment
 
-st.text('DataRobot Summer Intern Hackathon 2024')
-st.title("Generative Wrangler Operations")
-st.write("**Julius Stein & Lucas Ferreira**")
+st.set_page_config(layout="wide")
+st.text("Intern Hackathon - Summer 2024 - Julius Stein & Lucas Ferreira")
+st.image('images/datarobot_logo.png', width=320)
+st.subheader("Generative Wrangler Operations")
 
 col1, col2, col3, col4, col5, col6 = st.columns(6)
 with col1:
-    button1 = st.button('Button 1')
+    summarize_button = st.button('Summarize Text')
 with col2:
-    button2 = st.button('Button 2')
+    rephrase_button = st.button('Rephrase Text')
 with col3:
-    button3 = st.button('Button 3')
+    categorize_button = st.button('Label Categories')
 with col4:
-    button4 = st.button('Button 4')
+    variable_button = st.button('Encode Variables')
 with col5:
-    button5 = st.button('Button 5')
+    translate_button = st.button('Translate Text')
 with col6:
-    button6 = st.button('Button 6')
-
-if button1:
-    pass
-
-if button2:
-    pass
-
-if button3:
-    pass
-
-if button4:
-    pass
-
-if button5:
-    pass
-
-if button6:
-    pass
+    sentiment_button = st.button('Analyze Sentiment')
 
 df = pd.read_csv('data/movieReviews/test_reviews.csv')
-st.write(df)
+quotes = list(df['quote'])
+new_quotes = []
+
+if summarize_button:
+    for quote in quotes:
+        new_quotes.append(summarize_text(quote, 30))
+    df['summary'] = new_quotes
+
+if rephrase_button:
+    for quote in quotes:
+        new_quotes.append(rephrase_text(quote, 'friendly', 30))
+    df['rephrased'] = new_quotes
+
+if categorize_button:
+    for quote in quotes:
+        new_quotes.append(categorize_text(quote, ['positive', 'negative'], True))
+    df['categories'] = new_quotes
+
+if variable_button:
+    for quote in quotes:
+        new_quotes.append(encode_variables(quote, ['movie', 'actor', 'director']))
+    df['variable_encodings'] = new_quotes
+
+if translate_button:
+    for quote in quotes:
+        new_quotes.append(translate_text(quote, 'en', 'es'))
+    df['translated_en_es'] = new_quotes
+
+if sentiment_button:
+    for quote in quotes:
+        new_quotes.append(analyze_sentiment(quote, ['happy', 'sad', 'angry', 'neutral']))
+    df['sentiment_analysis'] = new_quotes
+
+st.dataframe(df)
