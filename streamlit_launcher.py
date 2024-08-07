261b26119f17ad0acbe981300e2d57bc4527e893
Julius Stein <julius.stein@datarobot.com>
2024-08-07 10:03:25 -0400

    bug fixes

--- streamlit_launcher.py
+++ streamlit_launcher.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import pandas as pd
 from generative_wrangling import summarize_message, rephrase_message, label_categories, encode_variables, translate_message, analyze_sentiment
-global df, button, feature, length, tone, title, exclusive, category1, category2, category3, variable1, variable2, variable3, inputLang, outputLang, emotion1, emotion2, emotion3, quotes
+
+st.set_page_config(layout="wide")
+st.text("Intern Hackathon - Summer 2024 - Julius Stein & Lucas Ferreira")
+st.image('images/datarobot_logo.png', width=320)
+st.subheader("Generative Wrangler Operations")
+st.session_state.df = None
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    feature = df.columns[0]
+    st.session_state.df = df
+    uploaded_file = None
 
 if 'summarize' not in st.session_state:
     st.session_state.summarize = False
@@ -64,47 +76,37 @@ def display_subfields_sentiment():
     st.session_state.translate = False
     st.session_state.sentiment = True
 
-st.set_page_config(layout="wide")
-st.text("Intern Hackathon - Summer 2024 - Julius Stein & Lucas Ferreira")
-st.image('images/datarobot_logo.png', width=320)
-st.subheader("Generative Wrangler Operations")
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    feature = df.columns[0]
-
-    col1, col2, col3, col4, col5, col6 = st.columns(6)
-    with col1:
-        summarize_button = st.button('Summarize Text', on_click=display_subfields_summarize)
-    with col2:
-        rephrase_button = st.button('Rephrase Text', on_click=display_subfields_rephrase)
-    with col3:
-        categorize_button = st.button('Label Categories', on_click=display_subfields_categorize)
-    with col4:
-        variable_button = st.button('Encode Variables', on_click=display_subfields_variable)
-    with col5:
-        translate_button = st.button('Translate Text', on_click=display_subfields_translate)
-    with col6:
-        sentiment_button = st.button('Analyze Sentiment', on_click=display_subfields_sentiment)
+col1, col2, col3, col4, col5, col6 = st.columns(6)
+with col1:
+    summarize_button = st.button('Summarize Text', on_click=display_subfields_summarize)
+with col2:
+    rephrase_button = st.button('Rephrase Text', on_click=display_subfields_rephrase)
+with col3:
+    categorize_button = st.button('Label Categories', on_click=display_subfields_categorize)
+with col4:
+    variable_button = st.button('Encode Variables', on_click=display_subfields_variable)
+with col5:
+    translate_button = st.button('Translate Text', on_click=display_subfields_translate)
+with col6:
+    sentiment_button = st.button('Analyze Sentiment', on_click=display_subfields_sentiment)
 
 #df = pd.read_csv('data/movieReviews/test_reviews.csv')
 #df = pd.read_csv('data/jira_bugs/jira_bugs.csv')
 #quotes = list(df['Description'])
 
 if st.session_state.summarize:
-    feature = st.text_input("Input Feature Name", df.columns[0])
+    feature = st.text_input("Input Feature Name", st.session_state.df.columns[0])
     length = st.number_input("Max Tokens", 10, 100, 30)
     title = st.text_input("New Feature Name", "new_feature")
     add_summarize_operation = st.button("Add Operation")
 elif st.session_state.rephrase:
-    feature = st.text_input("Input Feature Name", df.columns[0])
+    feature = st.text_input("Input Feature Name", st.session_state.df.columns[0])
     length = st.number_input("Max Tokens", 10, 100, 30)
-    tone = st.selectbox("Tone", ['friendly', 'professional', 'sarcastic'])
+    tone = st.selectbox("Tone", ['friendly', 'professional', 'sarcastic', 'formal', 'casual', 'informative'])
     title = st.text_input("New Feature Name")
     add_rephrase_operation = st.button("Add Operation")
 elif st.session_state.categorize:
-    feature = st.text_input("Input Feature Name", df.columns[0])
+    feature = st.text_input("Input Feature Name", st.session_state.df.columns[0])
     inner1, inner2, inner3 = st.columns(3)
     with inner1:
         category1 = st.text_input("Category 1")
@@ -116,7 +118,7 @@ elif st.session_state.categorize:
     title = st.text_input("New Feature Name", "new_feature")
     add_categorize_operation = st.button("Add Operation")
 elif st.session_state.variable:
-    feature = st.text_input("Input Feature Name", df.columns[0])
+    feature = st.text_input("Input Feature Name", st.session_state.df.columns[0])
     inner1, inner2, inner3 = st.columns(3)
     with inner1:
         variable1 = st.text_input("Variable 1")
@@ -127,12 +129,12 @@ elif st.session_state.variable:
     title = st.text_input("New Feature Name", "new_feature")
     add_variable_operation = st.button("Add Operation")
 elif st.session_state.translate:
-    feature = st.text_input("Input Feature Name", df.columns[0])
+    feature = st.text_input("Input Feature Name", st.session_state.df.columns[0])
     outputLang = st.text_input("Output Language")
     title = st.text_input("New Feature Name", "new_feature")
     add_translate_operation = st.button("Add Operation")
 elif st.session_state.sentiment:
-    feature = st.text_input("Input Feature Name", df.columns[0])
+    feature = st.text_input("Input Feature Name", st.session_state.df.columns[0])
     inner1, inner2, inner3 = st.columns(3)
     with inner1:
         emotion1 = st.text_input("Emotion 1")
@@ -143,8 +145,9 @@ elif st.session_state.sentiment:
     title = st.text_input("New Feature Name", "new_feature")
     add_sentiment_operation = st.button("Add Operation")
 
-if uploaded_file is not None:
-    quotes = list(df[feature])
+if st.session_state.df is not None:
+    quotes = [str(quote) if quote is not None else "" for quote in list(st.session_state.df[feature])]
+    df = st.session_state.df
 
 if st.session_state.summarize:
     if add_summarize_operation:
@@ -153,6 +156,7 @@ if st.session_state.summarize:
             new_quotes.append(summarize_message(quote, length))
         df[title] = new_quotes
         st.session_state.summarize = False
+        st.session_state.df = df
 elif st.session_state.rephrase:
     if add_rephrase_operation:
         new_quotes = []
@@ -160,6 +164,7 @@ elif st.session_state.rephrase:
             new_quotes.append(rephrase_message(quote, tone, length))
         df[title] = new_quotes
         st.session_state.rephrase = False
+        st.session_state.df = df
 elif st.session_state.categorize:
     if add_categorize_operation:
         new_quotes = []
@@ -167,6 +172,7 @@ elif st.session_state.categorize:
             new_quotes.append(label_categories(quote, [category1, category2, category3], exclusive))
         df[title] = new_quotes
         st.session_state.categorize = False
+        st.session_state.df = df
 elif st.session_state.variable:
     if add_variable_operation:
         new_quotes = []
@@ -174,6 +180,7 @@ elif st.session_state.variable:
             new_quotes.append(encode_variables(quote, [variable1, variable2, variable3]))
         df[title] = new_quotes
         st.session_state.variable = False
+        st.session_state.df = df
 elif st.session_state.translate:
     if add_translate_operation:
         new_quotes = []
@@ -181,6 +188,7 @@ elif st.session_state.translate:
             new_quotes.append(translate_message(quote, outputLang))
         df[title] = new_quotes
         st.session_state.translate = False
+        st.session_state.df = df
 elif st.session_state.sentiment:
     if add_sentiment_operation:
         new_quotes = []
@@ -188,6 +196,7 @@ elif st.session_state.sentiment:
             new_quotes.append(analyze_sentiment(quote, [emotion1, emotion2, emotion3]))
         df[title] = new_quotes
         st.session_state.sentiment = False
+        st.session_state.df = df
 
-if uploaded_file is not None:
-    st.dataframe(df)
+if st.session_state.df is not None:
+    st.dataframe(st.session_state.df)
